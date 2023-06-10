6a03493bc6d9193292eb3d86e127dc229157d1d0
khatia ormotsadze <khatiaormotsadze99@gmail.com>
2023-06-10 22:58:30 +0400

    git commit -m"Modify NumberDivider.java" -m"Modify ThreeStuckVars.java" -m"Modify TimeConvertor.java"

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+
+        divide(5);
+        int result = toBeDivided / 5;
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+
+
     public void convert(float minutes) {
+
+         float result = minutes * 60;
+         convert(1);
+         System.out.println(minutes + " minutes is equal to " + result + " seconds");
+
     }
 }
